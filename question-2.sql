9759d657275136f97b5978ae636f01fcfc5c479c
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-10 14:58:26 -0700

    Update question-2.sql

--- question-2.sql
+++ question-2.sql
@@ -1,2 +1,7 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
-SELECT employees.FirstName||' '||employees.LastName AS SalesAgentFullName, InvoiceId FROM employees JOIN customers JOIN invoices ORDER BY EmployeeId;
+SELECT employees.FirstName||' '||employees.LastName AS SalesAgentFullName, invoices.InvoiceId, invoices.CustomerId, InvoiceDate, Total,
+BillingAddress, BillingCity, BillingState, BillingCountry, BillingPostalCode
+FROM employees 
+JOIN customers ON employees.EmployeeId = customers.SupportRepId
+JOIN invoices ON customers.CustomerId = invoices.CustomerId
+ORDER BY SalesAgentFullName;
