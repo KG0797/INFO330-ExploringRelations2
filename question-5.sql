73a55d5dd41232949134d575731f4b2d043cbe28
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-09 19:55:17 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,8 @@
 -- What was the most purchased track of 2013?
+
+SELECT tracks.Name AS TrackName, artists.Name AS ArtistName, COUNT(*) AS Purchases
+FROM invoice_items
+JOIN invoices, tracks, albums, artists WHERE InvoiceDate LIKE '2013%'
+GROUP BY TrackId
+ORDER BY Purchases DESC
+LIMIT 1;
