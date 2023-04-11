9c18def9a97351dd1bbbfb75a77e6bc7bccfeae8
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-11 08:50:51 -0700

    Update question-7.sql

--- question-7.sql
+++ question-7.sql
@@ -1,1 +1,3 @@
 -- Find tracks (id, name, and composer) that have never been purchased (that is, they aren't part of a line item in an invoice).
+
+SELECT tracks.TrackId, tracks.Name, tracks.Composer FROM tracks JOIN invoices
