a27cbd5e38ace9c16aa6a8cf300074e6a48e89ac
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-11 14:05:11 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -2,7 +2,7 @@
 
 SELECT tracks.Name AS TrackName, COUNT(*) AS Purchases
 FROM invoice_items
-JOIN invoices, tracks WHERE InvoiceDate LIKE '2013%'
+JOIN invoices, tracks WHERE InvoiceDate LIKE '%2013%'
 GROUP BY tracks.TrackId
 ORDER BY Purchases DESC
 LIMIT 1;
