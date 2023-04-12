64f78b01c9cf997713219b5e64a1bba8a3f3a2f0
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-11 20:34:03 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -2,7 +2,9 @@
 
 SELECT tracks.Name AS TrackName, COUNT(*) AS Purchases
 FROM invoice_items
-JOIN invoices, tracks WHERE InvoiceDate LIKE '%2013%'
+JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+JOIN tracks ON invoice_items.TrackId = tracks.TrackId
+WHERE strftime('%Y', InvoiceDate) = '2013'
 GROUP BY tracks.TrackId
 ORDER BY Purchases DESC;
 
