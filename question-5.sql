370ffa924e747716abe4d17be71832adb4547af5
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-11 21:41:07 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,6 +1,6 @@
 -- What was the most purchased track of 2013?
 
-SELECT tracks.Name AS TrackName, COUNT(*) AS Purchases
+SELECT tracks.Name, COUNT(*) AS Purchases
 FROM invoice_items
 JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
 JOIN tracks ON invoice_items.TrackId = tracks.TrackId
